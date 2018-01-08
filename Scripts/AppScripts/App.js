308ba295d85934fadbd46158817a800f140d53a4
Faendol <jacquetliam@gmail.com>
2018-01-08 08:32:29 -0500

    Did A Thing

---
+++ Scripts/AppScripts/App.js
@@ -0,0 +1,20 @@
+﻿var json;
+//$.getJSON("data.json", function (data) { json = data; initialize(); });
+
+window.onload = function () {
+
+
+
+    console.log("We Good");
+    var tablebody = document.getElementById("tableblody");
+    for (var i = 0; i < data.length; i++) {
+        var parentthing = document.createElement("tr");
+        tablebody.appendChild(parentthing);
+        
+        var child2 = document.createElement("td");
+        child2.innerText = data[i];
+        
+        parentthing.appendChild(child2);
+    }
+
+}
